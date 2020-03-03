382cbfe6a8be6389c739b0e2a49d9625c23c94c5
JoshuaOliver99 <56636418+JoshuaOliver99@users.noreply.github.com>
2020-03-03 16:32:32 +0000

    Transfer push
    Basic implimentation of character controller
    Start on inventory system

---
+++ GAW 6 Secrets/Assets/Scripts/PlayerCotroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerCotroller : MonoBehaviour
+{
+    int speed = 5;
+    void Update()
+    {
+        keyboardInput();
+    }
+
+    private void keyboardInput()
+    {
+        // Move left or right
+        if (Input.GetKey("a") && !Input.GetKey("d"))
+            transform.position += new Vector3(-1 * speed * Time.deltaTime, 0, 0);
+        else if (Input.GetKey("d") && !Input.GetKey("a"))
+            transform.position += new Vector3(1 * speed * Time.deltaTime, 0, 0);
+
+        // Move up or down
+        if (Input.GetKey("w") && !Input.GetKey("s"))
+            transform.position += new Vector3(0, 1 * speed * Time.deltaTime, 0);
+        else if (Input.GetKey("s") && !Input.GetKey("w"))
+            transform.position += new Vector3(0, -1 * speed * Time.deltaTime, 0);
+    }
+}
---
+++ GAW 6 Secrets/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField] GameObject inventoryPanel;
+
+    private void Start()
+    {
+        inventoryPanel.SetActive(false);
+    }
+    void Update()
+    {
+        invetoryManager();
+    }
+
+
+    private void invetoryManager()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (inventoryPanel.activeSelf == true)
+                inventoryPanel.SetActive(false);
+            else
+                inventoryPanel.SetActive(true);
+        }
+
+    }
+}
